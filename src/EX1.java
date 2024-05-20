a193a7adff7df5b69e9a28627e41b6e7c556393b
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-05-20 16:54:37 +0200

    first commit

---
+++ src/EX1.java
@@ -0,0 +1,19 @@
+public class EX1 {
+    public static void main(String[] args) {
+        System.out.println("ciao");
+        System.out.println();
+
+    }
+
+    public static int product(int num1, int num2) {
+
+
+        return num1 + num2;
+    }
+
+    public static String concat(String example1, int example2){
+        return example1 + example2;
+        
+
+}
+}
---
+++ src/EX2.java
@@ -0,0 +1,5 @@
+public class EX2 {
+    public static void main(String[] args) {
+
+    }
+}
