ce061a5f050a9873c64881dbbbcb0008d8bc51f8
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-05-20 19:33:22 +0200

    comm ex 3

--- src/EX1.java
+++ src/EX1.java
@@ -2,6 +2,16 @@ public class EX1 {
     public static void main(String[] args) {
         System.out.println(product(2,5));
         System.out.println(concat("ciao", 3));
+        String[] arrayString = {"marco", "andrea", "giuseppe", "paolo", "alessia"};
+        String text = "CIAO";
+
+        String[] resultArray = unione(arrayString, text);
+
+
+        for (int i = 0; i < resultArray.length; i++) {
+
+            System.out.println(resultArray[i]);
+        }
 
     }
 
@@ -19,6 +29,21 @@ public class EX1 {
 
 public static String[] unione(String[] arrayString, String text){
         String[] newArray = new String[6];
+        for(int i = 0 ; i < newArray.length ; i++){
+            if (i < 2) {
+
+                newArray[i] = arrayString[i];
+            } else if (i == 2) {
+                newArray[i] = text;
+            } else {
+                newArray[i] = arrayString[i -1];
+            }
+
+
+
+
+        }
                 return newArray;
+
 }
 }
--- src/EX3.java
+++ src/EX3.java
@@ -1,5 +1,21 @@
 public class EX3 {
     public static void main(String[] args) {
+        System.out.println(perimetroRettangolo(10, 2.5));
+        System.out.println(evenOdd(5));
+        System.out.println(areaTriangolo(2.5, 4.2, 5.6));
 
+    }
+
+    public static double perimetroRettangolo(double l1, double l2) {
+        return l1 + l2 + l1 + l2;
+    }
+
+    public static int evenOdd(int myNumber) {
+        return myNumber % 2 == 0 ? 0 : 1;
+    }
+
+    public static double areaTriangolo(double l1, double l2, double l3) {
+        double semiPerimeter = (l1 + l2 + l3) / 2;
+        return Math.sqrt(semiPerimeter * (semiPerimeter - l1) * (semiPerimeter - l2) * (semiPerimeter - l3));
     }
 }
